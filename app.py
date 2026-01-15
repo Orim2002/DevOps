270adc5d26a5ffd09402391bacd918df97b1934b
orim2002 <o2002mm@gmail.com>
2026-01-15 22:06:44 +0200

    change text

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "<h1>Hello DevOps World! My CI/CD pipeline is coming...</h1>"
+    return "<h1>Success! Automation is Magic!</h1>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
