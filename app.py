f56c228329cba00aea70ab75649e72d78b0d6c6f
orim2002 <o2002mm@gmail.com>
2026-01-15 21:52:02 +0200

    add app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "<h1>Hello DevOps World! My CI/CD pipeline is coming...</h1>"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
